89df88bf4f3ec5d530b63e7e762289b9c61e7093
Attila Max Ruf <therufa@gmail.com>
2016-06-14 20:07:37 +0200

    added examples

---
+++ examples/uidemo/js/modules/index.js
@@ -0,0 +1,39 @@
+window.Guido = window.Guido || {};
+
+window.Guido.Index = (function ($, _) {
+
+  var module = {
+
+    ACTIONS: {
+
+      INDEX: [
+        { action: '@sayHello', icon: 'plus', caption: "hello world" },
+      ]
+    },
+
+    defaultObject: {
+    },
+
+    tableOptions: {
+    },
+
+    buttons: {
+
+      sayHello: {
+        dataAction: '@sayHello',
+        btnClasses: 'btn-primary btn-ok',
+        caption: 'CAP_BTN_SAVE'
+      }
+    },
+
+    // Actions
+    index: function() {
+      this._index();
+      console.log('index');
+      alert('index')
+    }
+    
+  };
+
+  return module;
+})(jQuery, _);
