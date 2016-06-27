af5af02160d435bab5653629d8149db903ddd752
Christoph Ambichl <christoph.ambichl@documatrix.com>
2016-06-27 15:17:17 +0200

    chore(setup): added dev dependencies

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var Guido = require('../src/index');
+
+describe('Guido', function() {
+
+  it('should work', function() {
+    expect( true ).to.be.true;
+  })
+});
